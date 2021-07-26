e5f90d2cb01e23e0eef4ffc0fbd51f8ef2d75d5e
finnolin <finnolin@gmail.com>
2021-07-26 11:14:03 +0200

    update

--- floors.lua
+++ floors.lua
@@ -2,8 +2,15 @@ local tArgs = { ... }
 
 local action = tArgs[1]
 
-function load(savedFloors)
-    local file = fs.open("savedFloors","r")
+function save(table, name)
+    local file = fs.open (name,"w")
+    file.write(textutils.serialize (table))
+    file.close()
+    print(" floors initiated ")
+end
+
+function load(name)
+    local file = fs.open(name,"r")
     local data = file.readAll()
     file.close()
     return textutils.unserialize(data)
@@ -16,5 +23,11 @@ if "new" == action then
     local newFloor = read()
     print("Floor Level")
     local newFloorLevel = read()
+
+    table.insert(tFloors, {floorName = newFloor, floorLevel= newFloorLevel})
+
+    for i=1, #tFloors do
+        Print(tFloors[i].floorName)
+    end
 
 end
